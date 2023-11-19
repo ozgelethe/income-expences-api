c77aebec5e69ab69431e18b53232e5daad8755f9
rukiyearican <rukiyearican@gmail.com>
2023-11-19 12:07:14 +0300

    added User class to models, added AUTH_USER_MODEL to settings

--- incomeexpencesapi/authentication/models.py
+++ incomeexpencesapi/authentication/models.py
@@ -34,3 +34,26 @@ class UserManager(BaseUserManager):
         user.save()
         
         return user
+
+        
+        
+class User(AbstractBaseUser, PermissionsMixin):
+    
+    username=models.CharField(max_length=255, unique=True, db_index=True)
+    email=models.EmailField(max_length=255, unique=True, db_index=True)
+    is_verified = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+    
+    objects = UserManager()
+    
+    def __str__(self):
+        return str(self.email)
+    
+    def tokens(self):
+        return ''
