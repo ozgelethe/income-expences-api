b01782b41db3b4a0e317faa7a2bdbb196f09e4f3
rukiyearican <rukiyearican@gmail.com>
2023-11-25 16:19:19 +0300

    create a token for the user

--- incomeexpencesapi/authentication/views.py
+++ incomeexpencesapi/authentication/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from rest_framework import generics, status
 from .serializers import RegisterSerializer
 from rest_framework.response import Response
-
+from rest_framework_simplejwt.tokens import RefreshToken
+from .models import User
 
 # Create your views here.
 
@@ -18,5 +19,9 @@ class RegisterView(generics.GenericAPIView):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         user_data = serializer.data
+        
+        user = User.objects.get(email=user_data['email'])
+        
+        token = RefreshToken.for_user(user)
         
         return Response(user_data, status=status.HTTP_201_CREATED)
--- incomeexpencesapi/incomeexpencesapi/settings.py
+++ incomeexpencesapi/incomeexpencesapi/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "authentication",
+    "rest_framework_simplejwt",
 ]
 
 MIDDLEWARE = [
